d03071f625f7097290bddc7801f5d88729b41d39
Jayson Cena <jayson.cena@clusterfoundry.com>
2013-07-10 08:19:02 -0500

    20-storage_replication.sh

--- bin/20-storage_replication.sh
+++ bin/20-storage_replication.sh
@@ -11,3 +11,10 @@ SCRIPT_NAME=`basename $0`
 source "$CLUSTER_CONFIG_FILE" || exit 2
 # load functions
 source $SCRIPT_LIBRARY || exit 2
+
+$CLUSTER_HOME/bin/21-install_csync2.sh || exitcode 1 "Error running $CLUSTER_HOME/bin/21-install_csync2.sh"
+$CLUSTER_HOME/bin/22-config_csync2.sh || exitcode 1 "Error running $CLUSTER_HOME/bin/22-config_csync2.sh"
+$CLUSTER_HOME/bin/23-initial_sync.sh || exitcode 1 "Error running $CLUSTER_HOME/bin/23-initial_sync.sh"
+
+msg "$SCRIPT_NAME: SCRIPT OK"
+exit 0
