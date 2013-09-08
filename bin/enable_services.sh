9321c727de16fba25b07cd635e2fea2fd3c76775
Jayson Cena <jayson.cena@clusterfoundry.com>
2013-09-08 14:44:24 +0000

    BUGFIX: does not enable apache and mysql after completion of all scripts.

--- bin/enable_services.sh
+++ bin/enable_services.sh
@@ -4,7 +4,6 @@ DEBIAN_FRONTEND=noninteractive
 # quick fix: change mysql server
 # for drupal
 sed "s/'127.0.0.1'/'$1'/Ig" -i /home/clients/websites/*/public_html/*/sites/default/settings.php
-exit 0
 
 # enable apache2
 update-rc.d apache2 enable
